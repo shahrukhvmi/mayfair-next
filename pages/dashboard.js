36c8c3dbeed4ff0b5245b4d9debd21a899b7633b
shahrukhvmi <sujaform2024@gmail.com>
2025-04-22 18:37:39 +0500

    update

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,38 +1,18 @@
+import MyAccount from '@/Components/MyAccount/MyAccount'
+import DashBoardLayout from '@/layout/Dashboard/dashBoardLayout'
+import React from 'react'
 
-import React from 'react';
-import DashBoardLayout from '@/layout/Dashboard/dashBoardLayout';
-import MyAccount from '@/Components/MyAccount/MyAccount';
+const dashboard = () => {
+    return (
+        <>
 
-const Dashboard = ({ data }) => {
-  return (
-    <DashBoardLayout>
-      <MyAccount data={data} />
-    </DashBoardLayout>
-  );
-};
 
-export async function getServerSideProps() {
-  try {
-    const res = await fetch('https://staging.mayfairweightlossclinic.co.uk/api/products/GetAllProducts');
-    if (!res.ok) {
-      throw new Error('Failed to fetch');
-    }
+            <DashBoardLayout>
+                <MyAccount />
+            </DashBoardLayout>
 
-    const json = await res.json();
-
-    return {
-      props: {
-        data: json?.data || null,
-      },
-    };
-  } catch (error) {
-    console.error('Fetch error:', error.message);
-    return {
-      props: {
-        data: null,
-      },
-    };
-  }
+        </>
+    )
 }
 
-export default Dashboard;
+export default dashboard
